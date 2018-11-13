6eaa4e21f82802066d56fc878cc280b329beb83e
yulu zhang <yulu@70kft.com>
2018-11-12 22:00:51 -0600

    update nav

--- craft/config/db.php
+++ craft/config/db.php
@@ -23,11 +23,5 @@ return array(
         'password' => 'Prisspery+18',
         'database' => 'pri',
         'initSQLs' => array("SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")        
-    ),
-    'eventsbythelab.com' => array(
-        'server' => 'localhost',
-        'user' => 'the-lab',
-        'password' => '@Z4?zDs%',
-        'database' => 'pri',
     )
 );
